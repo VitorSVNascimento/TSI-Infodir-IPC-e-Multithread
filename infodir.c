90edab68519e92065a2accb7ade75ea3f1049f0d
VitorNasc8 <vitornasc8k13@gmail.com>
2022-11-28 21:55:18 -0300

    Lógica que obtem o tamanho do diretorio
    Finalizaçao da logica que obtem o tamanho total do diretório passando por cada subdiretório

--- infodir.c
+++ infodir.c
@@ -2,29 +2,63 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include<unistd.h> 
 #include "infodir.h"
+
+unsigned long lerDiretorio(char *path,DIR *dir){
+    unsigned long total=0,retorno;
+    Diretorio *diretorio;
+    struct stat st;
+    if(abrirDiretorio(path,&dir)==FALHA){
+        return DIRETORIO_INEXISTENTE;
+    }else{
+        chdir(path);
+        while((diretorio = readdir(dir))){
+            if(strcmp(diretorio->d_name,"..") && strcmp(diretorio->d_name,".")){
+
+                if(diretorio->d_type==DT_DIR){
+                    retorno = lerDiretorio(diretorio->d_name,dir);
+                    if(retorno!=DIRETORIO_INEXISTENTE){
+                        chdir("..");
+                        total+=retorno;
+                    }
+                }else{
+                        total+=st.st_size;
+                        printf("\n%s\t%ld",diretorio->d_name,st.st_size);
+                    
+                }
+            }
+        }
+        return total;
+    }
+}
+
+int abrirDiretorio(char *path,DIR **diretorio){
+    
+    *diretorio = opendir(path);
+    if(!*diretorio)
+        return FALHA;
+    return SUCESSO;
+
+}
 
 int inicializa(int argc, char* argv[]){
     char path[TAMANHO_MAXIMO_DO_PATH];
-    DIR *dd;
-    Diretorio *d;
-    struct stat st;
+    DIR *dir;
+    unsigned long retorno;
     const int PRIMEIRO_PARAMETRO = 1;
     if(argc <= PRIMEIRO_PARAMETRO){
         strcpy(path,"./");
-    }
-    
-    dd = opendir(path);
-    if(dd==NULL){
-        printf("Não abriu");
-        return EXIT_FAILURE;
-    }
-    else
-        while((d = readdir(dd))){
-            stat(d->d_name,&st);
-            printf("\n%s\t%ld\n",d->d_name,st.st_size);
+    }else
+        strcpy(path,argv[PRIMEIRO_PARAMETRO]);
 
-        }
+    dir = opendir(path);
+    struct stat st;
+    stat("teste/teste.txt",&st);
+    printf("Tamanho teste %lu",st.st_size);
+
+    retorno = lerDiretorio(path,dir);
+    printf("\nRetorno total = %lu\n",retorno);
     return EXIT_SUCCESS;
 }
 
--- infodir.h
+++ infodir.h
@@ -1,5 +1,13 @@
 #pragma once
 #include <dirent.h>
 #define TAMANHO_MAXIMO_DO_PATH 500
+#define SUCESSO 1
+#define FALHA 0
+#define DIRETORIO_INEXISTENTE -1
 
 typedef struct dirent Diretorio;
+
+/*Efetua a abertura de um diretorio,
+*/
+int abrirDiretorio(char *path,DIR **diretorio);
+int inicializa(int argc, char* argv[]);
