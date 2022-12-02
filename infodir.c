7529bf22be6edd1e030473f3f3269178e888d836
VitorNasc8 <vitornasc8k13@gmail.com>
2022-12-02 10:14:11 -0300

    Logica de processos filhos
    Criação da logica de leitura através de processos filhos

--- infodir.c
+++ infodir.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
-#include<unistd.h> 
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <unistd.h> 
+#include <locale.h> 
 #include "infodir.h"
 
 unsigned long lerDiretorio(char *path,DIR *dir){
@@ -14,17 +17,19 @@ unsigned long lerDiretorio(char *path,DIR *dir){
     }else{
         chdir(path);
         while((diretorio = readdir(dir))){
-            if(strcmp(diretorio->d_name,"..") && strcmp(diretorio->d_name,".")){
+            if(strcmp(diretorio->d_name,"..") && strcmp(diretorio->d_name,".") ){
 
                 if(diretorio->d_type==DT_DIR){
                     retorno = lerDiretorio(diretorio->d_name,dir);
                     if(retorno!=DIRETORIO_INEXISTENTE){
                         chdir("..");
                         total+=retorno;
+                        
                     }
                 }else{
+                        stat(diretorio->d_name,&st);
                         total+=st.st_size;
-                        printf("\n%s\t%ld",diretorio->d_name,st.st_size);
+                        printf("\n%s\t%ld\n%d",diretorio->d_name,st.st_size,diretorio->d_type);
                     
                 }
             }
@@ -45,22 +50,93 @@ int abrirDiretorio(char *path,DIR **diretorio){
 int inicializa(int argc, char* argv[]){
     char path[TAMANHO_MAXIMO_DO_PATH];
     DIR *dir;
-    unsigned long retorno;
+    
+    
     const int PRIMEIRO_PARAMETRO = 1;
     if(argc <= PRIMEIRO_PARAMETRO){
         strcpy(path,"./");
     }else
         strcpy(path,argv[PRIMEIRO_PARAMETRO]);
 
-    dir = opendir(path);
-    struct stat st;
-    stat("teste/teste.txt",&st);
-    printf("Tamanho teste %lu",st.st_size);
-
-    retorno = lerDiretorio(path,dir);
-    printf("\nRetorno total = %lu\n",retorno);
+    if(abrirDiretorio(path,&dir)==SUCESSO)
+        processoPai(dir,path);
+    
+    
     return EXIT_SUCCESS;
 }
+
+int processoPai(DIR *dir,char *path){
+    int teste=0;
+    Diretorio *diretorio;
+    struct stat st;
+    int segmentoID = shmget(IPC_PRIVATE, sizeof(Infodir), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    Infodir *infodirPtr = malloc(sizeof(Infodir));
+    infodirPtr= (Infodir *)shmat(segmentoID, NULL, 0);
+    Infodir infodir;
+   
+    chdir(path);
+    while((diretorio = readdir(dir))){
+        if(strcmp(diretorio->d_name,"..") && strcmp(diretorio->d_name,".") ){
+
+            if(diretorio->d_type==DT_DIR){
+                teste=1;
+                pid_t pid = criarProcesso();
+                if(!pid)
+                    processoFilho(diretorio->d_name,segmentoID);
+            }else{
+                    stat(diretorio->d_name,&st);
+                    infodir.tamanhoEmBytes+=st.st_size;
+                    printf("\n%s\t%ld\n%d\n",diretorio->d_name,st.st_size,diretorio->d_type);
+                
+            }
+        }
+    }
+    if(teste)
+        infodir.tamanhoEmBytes += infodirPtr->tamanhoEmBytes;
+    printf("\nRetorno total = %llu\n",infodir.tamanhoEmBytes);
+    shmctl(segmentoID, IPC_RMID, NULL); 
+    return EXIT_SUCCESS;
+}
+
+int processoFilho(char *path,int segmentoID){
+    printf("\nEntrei no filho = %s\n",path);
+
+    DIR *dir;
+    unsigned long retorno,total=0;
+    abrirDiretorio(path,&dir);
+    printf("\nAntes %s\n",path);
+    retorno = lerDiretorio(path,dir);
+    printf("\nDepois %s\n",path);
+    if(retorno!=DIRETORIO_INEXISTENTE){
+         chdir("..");
+        total+=retorno;       
+    }
+    printf("\nRetorno %lu\n",total);
+    Infodir* infodirPtr= (Infodir *)shmat(segmentoID, NULL, 0);
+    printf("\nValor do criação %llu\n",infodirPtr->tamanhoEmBytes);
+    infodirPtr->tamanhoEmBytes+=total;
+    printf("\nValor do infodir %llu\n",infodirPtr->tamanhoEmBytes);
+    shmdt(infodirPtr);
+
+    exit(EXIT_SUCCESS);
+    // return 0;
+}
+
+pid_t criarProcesso() {
+    // Cria o processo filho.
+    pid_t pid = fork();
+
+    if(pid>0)
+        wait(NULL);
+    // Verifica se ocorreu um erro na criação do processo filho. 
+    if (pid < 0)
+    {
+        printf("\nERRO: o processo filho não foi criado.\n\n");
+        exit(EXIT_FAILURE);
+    }
+    return pid;
+}
+
 
 int main(int argc, char* argv[]){
     return inicializa(argc,argv);
--- infodir.h
+++ infodir.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <dirent.h>
+#include <sys/types.h>
 #define TAMANHO_MAXIMO_DO_PATH 500
 #define SUCESSO 1
 #define FALHA 0
@@ -7,7 +8,17 @@
 
 typedef struct dirent Diretorio;
 
+typedef struct{
+    char nomeDir;
+    unsigned long long tamanhoEmBytes;
+    unsigned int numeroDeSubdiretorios;
+    unsigned int numeroDeArquivos;
+}Infodir;
+
 /*Efetua a abertura de um diretorio,
 */
 int abrirDiretorio(char *path,DIR **diretorio);
 int inicializa(int argc, char* argv[]);
+int processoPai(DIR *dir,char *path);
+int processoFilho(char *path,int segmentoID);
+pid_t criarProcesso();
